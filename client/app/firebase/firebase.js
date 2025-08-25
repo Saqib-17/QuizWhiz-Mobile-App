31cef2cf82a770448caa960e44021dc047ebeef6
Saqib-17 <sakib17shahidul@gmail.com>
2025-08-25 18:06:22 +0600

    fixed firebase credential for android apps

--- client/app/firebase/firebase.js
+++ client/app/firebase/firebase.js
@@ -1,26 +1,32 @@
+// app/firebase/firebase.js
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyApkOKlnOoMPdIvNep715mVGrkXSW1lnLQ",
+  apiKey: "AIzaSyC-t-YcECSnKn056uGjo0UTZm_0PUT8Zk4",
   authDomain: "quiz-whiz-mobile.firebaseapp.com",
   projectId: "quiz-whiz-mobile",
   storageBucket: "quiz-whiz-mobile.firebasestorage.app",
   messagingSenderId: "1093621110583",
-  appId: "1:1093621110583:ios:2d9ff15e2e9361d689ecb2"
+  appId: "1:1093621110583:android:92e8b27cbcb7717389ecb2"
 };
 
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
-// ✅ Auth with persistent storage
+// ✅ Auth with persistent storage using AsyncStorage
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  persistence: getReactNativePersistence(AsyncStorage),
 });
 
+// Firestore instance
 export const db = getFirestore(app);
+
+// Storage instance
 export const storage = getStorage(app);
 
 export default app;
